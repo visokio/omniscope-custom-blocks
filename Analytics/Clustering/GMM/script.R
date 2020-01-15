c63c244f3f5596d5623b881387527142ced28608
Nils Drechsel <nils@visokio.com>
2020-01-15 14:45:38 +0000

    add GMM

---
+++ Analytics/Clustering/GMM/script.R
@@ -0,0 +1,51 @@
+library(omniscope)
+
+omniscope = Omniscope()
+
+input.data = read.input.records(omniscope, input.number=1)
+input.data.2 = read.input.records(omniscope, input.number=2)
+
+### Input fields
+
+fields.to.use <- get.option(omniscope, "fieldsToUse")
+
+### Parameters
+
+use.all.numeric.fields <- get.option(omniscope, "useAllNumericFields")
+
+### Script
+
+
+library(mclust)
+
+if (is.null(input.data))
+  stop("No input data")
+
+if (use.all.numeric.fields) {
+  fields.to.use <- names(input.data)[sapply(input.data, is.numeric)]
+}
+
+# Calculate the clustering
+model <- Mclust(input.data[,fields.to.use])
+
+output.data <- cbind(input.data, model$classification)
+names(output.data) <- c(names(input.data), "Cluster")
+
+if (!is.null(input.data.2)) {
+
+  # Cluster the unseen data
+  new.data.to.cluster <- input.data.2[,fields.to.use]
+  new.assignments <- predict(model, newdata=new.data.to.cluster)$classification
+
+  output.data.2 <- cbind(input.data.2, new.assignments)
+  names(output.data) <- c(names(input.data), "Cluster")
+
+}
+
+if (!is.null(output.data)) {
+  write.output.records(omniscope, output.data, output.number=1)
+}
+if (!is.null(output.data.2)) {
+  write.output.records(omniscope, output.data.2, output.number=2)
+}
+close(omniscope)
