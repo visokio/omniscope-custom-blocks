89c2189bf7fc7f2c83c131b9cff58d65d3c86818
Nils Drechsel <nils@visokio.com>
2020-01-15 17:27:03 +0000

    adding airlines, flights and overpass

---
+++ Connectors/Flightstats/Flights/script.R
@@ -0,0 +1,84 @@
+library(omniscope)
+
+omniscope = Omniscope()
+
+input.data = read.input.records(omniscope, input.number=1)
+
+### Input fields
+
+flight.carrier.field <-get.option(omniscope, "carrierField")
+flight.number.field <- get.option(omniscope, "flightNumberField")
+flight.date.field <- get.option(omniscope, "dateField")
+
+### Parameters
+
+flightstats.app.id <- get.option(omniscope, "appID")
+
+flightstats.app.key <- get.option(omniscope, "appKey")
+
+### Script
+
+
+library(httr)
+library(jsonlite)
+library(data.table)
+library(dplyr)
+library(lubridate)
+
+# sanity checks
+if (is.null(input.data)) stop("No input data")
+
+# extract relevant fields from data
+flights <- data.table(carrier=input.data[, flight.carrier.field], number=input.data[, flight.number.field], date=input.data[, flight.date.field])
+
+
+app.id.string <- paste("appId", flightstats.app.id, sep="=")
+app.key.string <- paste("appKey", flightstats.app.key, sep="=")
+
+app.info <- paste(app.id.string, app.key.string, sep="&")
+
+
+base <- "https://api.flightstats.com"
+endpoint <- "flex/schedules/rest/v1/json/flight"
+
+
+output.data <- NULL
+
+# iterate through flights and request info from flighstats
+for (i in 1:nrow(flights)) {
+  
+  carrier = flights$carrier[i]
+  number = flights$number[i]
+  departure.day = day(flights$date[i])
+  departure.month = month(flights$date[i])
+  departure.year = year(flights$date[i])
+  
+  # make sure we don't send garbage
+  if (is.na(carrier) || is.na(number) || is.na(departure.day) || is.na(departure.month) || is.na(departure.month)) next
+  
+  url.query <- paste(base, endpoint, carrier, number, "departing", departure.year, departure.month, departure.day, sep = "/")
+  url <- paste(url.query, app.info, sep = "?")
+  
+  result <- GET(url)
+  
+  # sanity check
+  if (result$status_code != 200) next
+  
+  json <- fromJSON(content(result, "text"), flatten = TRUE)
+  data <- as.data.frame(json$scheduledFlights)
+  
+  if (nrow(data) == 0) next
+  
+  # append results
+  if (is.null(output.data)) {
+    output.data <- data
+  } else {
+    output.data <- bind_rows(output.data, data)
+  }
+  
+}
+
+if (!is.null(output.data)) {
+  write.output.records(omniscope, data.frame(output.data), output.number=1)
+}
+close(omniscope)
---
+++ Connectors/Overpass/Street Coordinates/script.py
@@ -0,0 +1,50 @@
+from omniscope import Omniscope
+import pandas
+
+omniscope = Omniscope()
+
+# read the records associated to the first block input
+input_data = omniscope.read_input_records(input_number=0)
+
+### Info
+#  - https://python-overpy.readthedocs.io/en/latest/example.html#use-overpass-ql-or-overpass-xml
+#  - http://overpass-turbo.eu/
+#  - https://wiki.openstreetmap.org/wiki/Overpass_API/Overpass_API_by_Example#List_of_streets
+
+### Input fields
+address_field_name = omniscope.get_option("addressField")
+
+### Script
+import overpy
+import time
+
+# sanity checks
+if (is.null(input.data)) stop("No input data")
+
+api = overpy.Overpass()
+
+# Define output columns
+output_data = pandas.DataFrame(columns=[address_field_name,'Path','Longitude','Latitude'])
+
+i = 0
+for index, row in input_data.iterrows():
+    finished = False
+    while not finished:
+        try:
+            # Find all the streets that match the given name
+            result = api.query('way["name"~"'+row[address_field_name]+'"];(._;>;);out body;')
+            for way in result.ways:
+                # For each street found, create a new output row for each point with the street Id, Lat and Lon
+                for node in way.get_nodes():
+                    output_data.loc[i] = [row[address_field_name], way.id, str(node.lon), str(node.lat)]
+                    i += 1
+            finished = True
+        # Catch exception when too many requests and wait 10 seconds before repeating the query
+        except overpy.exception.OverpassTooManyRequests:
+            time.sleep(10)
+
+
+#write the output records in the first output
+if output_data is not None:
+    omniscope.write_output_records(output_data, output_number=0)
+omniscope.close()
