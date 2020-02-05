c32a59af207159e43619cca245cff065711ce0a2
Nils Drechsel <nils@visokio.com>
2020-02-05 17:32:58 +0000

    improving join result

--- Preparation/Join/Inequality Join/script.R
+++ Preparation/Join/Inequality Join/script.R
@@ -14,28 +14,33 @@ right.field = get.option(omni.api, "right")
 #sanity checks
 if (is.null(left)) abort(omni.api, "No 'left' input data")
 if (is.null(right)) abort(omni.api, "No 'right' input data")
-if (!is.numeric(left[, left.field])) abort(omni.api, "\"Left field\" must be numeric")
-if (!is.numeric(right[, right.field])) abort(omni.api, "\"Right field\" must be numeric")
+
 if ("left.field.internal.omniscope" %in% names(left)) abort(omni.api, "\"left.field.internal.omniscope\" is used internally by this block. The left input data cannot contain a field with this name.")
 if ("right.field.internal.omniscope" %in% names(right)) abort(omni.api, "\"right.field.internal.omniscope\" is used internally by this block. The right input data cannot contain a field with this name.")
 
 
+left$left.field.internal.omniscope = left[, left.field]
+right$right.field.internal.omniscope = right[, right.field]
+
 left <- left[complete.cases(left[, left.field]), ]
 right <- right[complete.cases(right[, right.field]), ]
+
+is.date <- function(x) {class(x)[1] %in% c("POSIXct", "POSIXt")}
+
+if (!is.numeric(left[, left.field]) && !is.date(left[, left.field])) abort(omni.api, "\"Left field\" must be numeric or date")
+if (!is.numeric(right[, right.field]) && !is.date(right[, right.field])) abort(omni.api, "\"Right field\" must be numeric or date")
+
+
 
 setDT(left)
 setDT(right)
 
-setnames(left, left.field, "left.field.internal.omniscope")
-setnames(right, right.field, "right.field.internal.omniscope")
 
-ex = str2lang(paste("left.field.internal.omniscope", comparator, "right.field.internal.omniscope", sep=" "))
+join.on = paste("left.field.internal.omniscope", comparator, "right.field.internal.omniscope", sep="")
 
-join.on = substitute(list(ex), list(ex = ex))
+output.data <- left[right, on=c(join.on), allow.cartesian = T, nomatch=0]
 
-output.data <- data.frame(left[right, on = eval(join.on), allow.cartesian = T, nomatch=0])
-
-setnames(output.data, "left.field.internal.omniscope", left.field)
+output.data[, c("left.field.internal.omniscope") := NULL]
 
 if (!is.null(output.data)) {
   write.output.records(omni.api, output.data, output.number=1)
