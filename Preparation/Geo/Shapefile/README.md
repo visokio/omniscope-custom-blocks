17ca19b9be512f49443606082dc8f7ace6f9d127
Nils Drechsel <nils.drechsel@gmail.com>
2021-01-13 11:45:43 +0000

    adding shapefile block

---
+++ Preparation/Geo/Shapefile/README.md
@@ -0,0 +1,28 @@
+# Shapefile
+
+Joins regions defined in a shapefile with points defined as latitudes and longitudes, and gives meta information about the content of the shapefile.
+
+## Language
+Python
+
+## Parameters
+### Shapefile
+The shapefile to use
+
+### Label field
+Property within the shapefile defining the name of a region
+
+### Geometry field
+Property within the shapefile defining the geometry of a region
+
+### Latitude field
+Field in the first input defining latitudes
+
+### Longitude field
+Field in the first input defining longitudes
+
+## Dependencies
+geopandas
+
+## Source
+[script.py](https://github.com/visokio/omniscope-custom-blocks/blob/master/Preparation/Geo/Shapefile/script.py)
---
+++ Preparation/Geo/Shapefile/script.py
@@ -0,0 +1,51 @@
+from omniscope.api import OmniscopeApi
+omniscope_api = OmniscopeApi()
+
+
+
+import fiona
+import geopandas as gp
+import pandas as pd
+
+
+shp_file = omniscope_api.get_option("shp_file")
+lat_field = omniscope_api.get_option("latitude_field")
+lon_field = omniscope_api.get_option("longitude_field")
+label = omniscope_api.get_option("label")
+geometry = omniscope_api.get_option("geometry")
+
+shp = gp.read_file(shp_file)
+shp_length = len(shp.index)
+
+omniscope_api.write_output_records(shp, output_number=1)
+
+if label is None or geometry is None or lat_field is None or lon_field is None:
+	omniscope_api.close()
+else:
+    input_data = pd.DataFrame(omniscope_api.read_input_records(input_number=0))
+    output_data = input_data.copy()
+    df = input_data.copy()
+
+
+    lat = df[lat_field]
+    lon = df[lon_field]
+
+
+    gdf = gp.GeoDataFrame(df, geometry=gp.points_from_xy(lon, lat))
+    gdf_length = len(gdf.index)
+
+    d = {True: 1, False: 0}
+
+    for i in range(0, shp_length):
+        shp_label = shp.iloc[i][label]
+        shp_geometry = shp.iloc[i][geometry]
+
+        mask = gdf.within(shp_geometry)
+        output_data[shp_label] = mask.map(d)
+
+
+
+    #write the output records in the first output
+    if output_data is not None:
+        omniscope_api.write_output_records(output_data, output_number=0)
+    omniscope_api.close()
