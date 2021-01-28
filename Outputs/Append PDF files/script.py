d1ff0fce1807635cb2135776f00f6f5e16370ec3
Pogs <antonio.poggi@ymail.com>
2021-01-28 18:22:40 +0000

    Add files via upload

---
+++ Outputs/Append PDF files/script.py
@@ -0,0 +1,30 @@
+from omniscope.api import OmniscopeApi
+from PyPDF2 import PdfFileMerger
+import glob
+
+omniscope_api = OmniscopeApi()
+
+# read the records associated to the first block input
+input_data = omniscope_api.read_input_records(input_number=0)
+
+pdfPath = omniscope_api.get_option("pdfPath")
+outputFolder = omniscope_api.get_option("outputFolder")
+
+merger = PdfFileMerger()
+
+for index, row in input_data.iterrows():
+    pdf = row[pdfPath]    
+    merger.append(pdf)
+
+outputFile = outputFolder+"/"+omniscope_api.get_option("fileName")
+
+merger.write(outputFile)
+merger.close()
+
+output_data = input_data
+output_data['Combined PDF'] = outputFile
+
+#write the output records in the first output
+if output_data is not None:
+    omniscope_api.write_output_records(output_data, output_number=0)
+omniscope_api.close()
