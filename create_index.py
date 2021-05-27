4b035ff704e873a8814b54ad3b4979b97480f598
nils-drechsel <nils.drechsel@gmail.com>
2021-05-27 11:51:56 +0100

    adding default category to refresh script

--- create_index.py
+++ create_index.py
@@ -17,10 +17,10 @@ supported_options_versions = [1]
 keys_required = {"name": "displayName",  "language": "language", "optionsVersion": "optionsVersion"}
 
 # if these are not found in the individual manifests, they are initialised with the value
-keys_with_defaults = {"tags": []}
+keys_with_defaults = {"tags": [], "category" : "Miscellaneous"}
 
 # optional keys, if not found in the individual manifests they are ignored
-keys_optional = ["description", "category", "subcategory", "icon"]
+keys_optional = ["description", "subcategory", "icon"]
 
 
 
@@ -31,35 +31,35 @@ import json
 def process_directory(path: str, blocks):
     with open(path+"/manifest.json", 'r') as manifest_file:
         manifest = json.load(manifest_file)
-        
+
         if not "optionsVersion" in manifest or not manifest["optionsVersion"] in supported_options_versions:
             return
-        
+
         for key in keys_required:
             if not key in manifest:
                 raise Exception(f"Key {key} is required but not present in manifest {path}")
 
         folder = os.sep.join(path.split(os.sep)[1:])
-        
+
         block = {"path": folder}
-        
-        
+
+
         for key in keys_required:
             block[keys_required[key]] = manifest[key]
-            
+
         for key in keys_with_defaults:
             if not key in manifest:
                 block[key] = keys_with_defaults[key]
             else:
                 block[key] = manifest[key]
-                
+
         for key in keys_optional:
             if key in manifest:
                 block[key] = manifest[key]
-        
+
         blocks.append(block)
-        
-        
+
+
 
 
 blocks = []
@@ -69,7 +69,7 @@ for root, dirs, files in os.walk("."):
     for file in files:
         if file == "manifest.json":
             process_directory(root, blocks)
-        
-        
+
+
 with open(index_file_name, 'w') as index_file:
     json.dump({"blocks": blocks}, index_file, indent=4)
