4552d23dda0caabef984d0b3f1d7f3dee614a52a
Nils Drechsel <nils.drechsel@gmail.com>
2021-09-28 11:36:52 +0100

    adding two more outputs to the field renamer

--- Preparation/Field Renamer/script.R
+++ Preparation/Field Renamer/script.R
@@ -16,6 +16,11 @@ if (length(current.names) !=length(unique(current.names))) abort(omni.api, "\"Cu
 new.names.field = get.option(omni.api, "newNames")
 new.names = input.data[, new.names.field]
 
+old.names = names(input.data.2)
+
+unused.names = data.frame(unused = setdiff(current.names, old.names))
+unchanged.names = data.frame(unchanged = setdiff(old.names, current.names))
+
 library(data.table)
 setDT(input.data.2)
 
@@ -24,5 +29,7 @@ setnames(input.data.2, current.names, new.names, skip_absent=TRUE)
 output.data = input.data.2
 if (!is.null(output.data)) {
   write.output.records(omni.api, output.data, output.number=1)
+  write.output.records(omni.api, unused.names, output.number=2)
+  write.output.records(omni.api, unchanged.names, output.number=3)
 }
 close(omni.api)
