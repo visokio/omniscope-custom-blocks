8e72015328be31a4902a3724b4d7c9a1b37dde85
Manuel <manuel@visokio.com>
2022-02-08 11:20:57 +0000

    Implementing pagination in Google BigQuery Import Table block

--- Connectors/BigQuery/Google BigQuery Import Table/script.py
+++ Connectors/BigQuery/Google BigQuery Import Table/script.py
@@ -5,10 +5,10 @@ import os
 from omniscope.api import OmniscopeApi
 from google.cloud import bigquery
 
-def read_big_query_table(table_id):
+def read_big_query_table(table_id, page_size):
     bqclient = bigquery.Client()
     table = bigquery.TableReference.from_string(table_id=table_id)
-    return bqclient.list_rows(table).to_dataframe()
+    return bqclient.list_rows(table, page_size = page_size).to_dataframe_iterable()
 
 
 def main():
@@ -17,9 +17,12 @@ def main():
         key_file = omniscope_api.get_option("ApplicationCredentials")
         if key_file is not None:
             os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = key_file
+        page_size = omniscope_api.get_option("PageSize")
+        if page_size is None:
+            page_size = 10_000
         table_id = omniscope_api.get_option("TableId")
-        data_frame = read_big_query_table(table_id=table_id)
-        omniscope_api.write_output(data_frame, 0)    
+        for data_frame in read_big_query_table(table_id=table_id, page_size = page_size):
+            omniscope_api.write_output(data_frame, 0)
         omniscope_api.close()
     except Exception as e:
         omniscope_api.cancel(str(e))
